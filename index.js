716fedf0f8bfd651264f2f5f0e903f8a0b0392ad
Dmitrii An <an.dmitriy93@gmail.com>
2021-03-04 17:26:15 +0600

    best white openings basic bar chart

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
-// const type = d => {
-//   return {
-//     rank: +d['Rank'],
-//     opening: d.Opening,
-//     win: +d['Win%'],
-//     draw: +d['Draw%'],
-//     points: +d['Points per 100 games']
-//   }
-// }
-
-// d3.csv('White best openings.csv', type)
-//   .then(d => {
-//     console.log(d)
-// })
-
-let openingsData;
-
-d3.csv('White best openings.csv', d => {
+let openings = d3.csvParse(`Rank,Opening,Win%,Draw%,Points per 100 games
+1,Queen's Gambit,40,36,58
+2,Blackmar Diemer Gambit,49,16,57
+3,Ruy Lopez,40,33,56.5
+4,Bishop's Opening,41,30,56
+5,Benko Opening,38,35,55.5
+5,Reti Opening,37,37,55.5
+5,Vienna Game,41,29,55.5
+8,Centre Game,44,22,55
+8,English Opening,38,34,55
+8,Scotch Game,40,30,55
+`, function(d){
   return {
     rank: +d['Rank'],
     opening: d.Opening,
     win: +d['Win%'],
     draw: +d['Draw%'],
     points: +d['Points per 100 games']
-  };
-}).then(data => {
-  openingsData = data;
+  }
 })
 
 
+const xScale = d3.scaleBand()   // ordinal scale every iteam have the same width
+    .domain(openings.map(dataPoint => dataPoint.opening))
+    .rangeRound([0, 500])       // tells from the array how much space is available from 0 to max width
+    .padding(0.1);              // find % padding between iteams
 
+const yScale = d3.scaleLinear() // allow to calculate right height as a value of data
+    .domain([0, 100])            // allow specify min and max value we able to map into chart
+    .range([300, 0]);           // actual available space in px
+
+const container = d3.select('svg')
+    .classed('container', true)
+
+const bars = container
+    .selectAll('.bar')
+    .data(openings)
+    .enter()
+    .append('rect')
+    .classed('bar', true)
+    .attr('width', xScale.bandwidth()) // the same as style in div, attr for svg elements
+    .attr('height', data => 300 - yScale(data.points)) // from max height we minus yScale to looks charts how we used to see
+    .attr('x', data => xScale(data.opening)) // set width basis on fucntion
+    .attr('y', data => yScale(data.points))
 
 
 
@@ -66,18 +79,3 @@ d3.csv('White best openings.csv', d => {
 //     .attr('x', data => xScale(data.region)) // set width basis on fucntion
 //     .attr('y', data => yScale(data.value))
 
-//---------------------------------------------------------------------------------------------------------
-
-// how to join data table
-// const countryData = {
-//     items: ['China', 'India', 'USA'],
-//     addItem(item) {
-//         this.items.push(item);
-//     },
-//     removeItem(index) {
-//         this.item.splice(index, 1);
-//     },
-//     updateItem(index, newItem) {
-//         this.items[index] = newItem;
-//     }
-// };
