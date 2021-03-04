e98af944755f3a522574f49429197583cd556028
Dmitrii An <an.dmitriy93@gmail.com>
2021-03-05 02:02:55 +0600

    white openings refactor

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let openings = d3.csvParse(`Rank,Opening,Win%,Draw%,Points per 100 games
+
+let openings = d3.csvParse(
+  `Rank,Opening,Win%,Draw%,Points per 100 games
 1,Queen's Gambit,40,36,58
 2,Blackmar Diemer Gambit,49,16,57
 3,Ruy Lopez,40,33,56.5
@@ -9,42 +11,87 @@ let openings = d3.csvParse(`Rank,Opening,Win%,Draw%,Points per 100 games
 8,Centre Game,44,22,55
 8,English Opening,38,34,55
 8,Scotch Game,40,30,55
-`, function(d){
-  return {
-    rank: +d['Rank'],
-    opening: d.Opening,
-    win: +d['Win%'],
-    draw: +d['Draw%'],
-    points: +d['Points per 100 games']
+`,
+  function (d) {
+    return {
+      rank: +d["Rank"],
+      opening: d.Opening,
+      win: +d["Win%"],
+      draw: +d["Draw%"],
+      points: +d["Points per 100 games"],
+    };
   }
-})
+);
+
+let margin = { top: 20, right: 20, bottom: 30, left: 40 };
+
+let height = 500 - margin.top - margin.bottom
+let width = 1160 - margin.left - margin.right
+
+let chart = d3.select('svg')
+    .attr("height", height + margin.top + margin.bottom)
+    .attr('width', width + margin.left + margin.right)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
 
-const xScale = d3.scaleBand()   // ordinal scale every iteam have the same width
-    .domain(openings.map(dataPoint => dataPoint.opening))
-    .rangeRound([0, 500])       // tells from the array how much space is available from 0 to max width
-    .padding(0.1);              // find % padding between iteams
+let x = d3.scaleBand().domain(openings.map(d => d.opening)).range([0, width]).padding(0.1)
+let y = d3.scaleLinear().domain([0, 100]).range([height, 0])
 
-const yScale = d3.scaleLinear() // allow to calculate right height as a value of data
-    .domain([0, 100])            // allow specify min and max value we able to map into chart
-    .range([300, 0]);           // actual available space in px
+// let barWidth = width / openings.length
 
-const container = d3.select('svg')
-    .classed('container', true)
+chart.append('g').attr('class', 'axis x')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(d3.axisBottom(x).ticks(10))
 
-const bars = container
-    .selectAll('.bar')
+chart.append('g').attr('class', 'axis y')
+    .call(d3.axisLeft(y).ticks(10))
+
+let bar = chart.selectAll('.bar')
     .data(openings)
     .enter()
-    .append('rect')
-    .classed('bar', true)
-    .attr('width', xScale.bandwidth()) // the same as style in div, attr for svg elements
-    .attr('height', data => 300 - yScale(data.points)) // from max height we minus yScale to looks charts how we used to see
-    .attr('x', data => xScale(data.opening)) // set width basis on fucntion
-    .attr('y', data => yScale(data.points))
+
+
+bar.append('rect')
+    .attr('class', 'bar')
+    .attr('x', (data) => x(data.opening))
+    .attr('y', (data) => y(data.points))
+    .attr('height',(data) => height - y(data.points))
+    .attr('width', x.bandwidth())
 
 
 
+
+// const xScale = d3
+//   .scaleBand() // ordinal scale every iteam have the same width
+//   .domain(openings.map((dataPoint) => dataPoint.opening))
+//   .rangeRound([0, 500]) // tells from the array how much space is available from 0 to max width
+//   .padding(0.1); // find % padding between iteams
+
+// const yScale = d3
+//   .scaleLinear() // allow to calculate right height as a value of data
+//   .domain([0, 100]) // allow specify min and max value we able to map into chart
+//   // .domain([0, d3.max(openings, d => d.points)])            // allow specify min and max value we able to map into chart
+//   .range([300, 0]); // actual available space in px
+
+// const container = d3.select("svg").classed("container", true);
+
+// const x_axis = d3.axisBottom().scale(xScale);
+
+// const svg = d3.select('svg')
+
+// const y_axis = d3.axisLeft().scale(yScale);
+
+// const bars = container
+//   .selectAll(".bar")
+//   .data(openings)
+//   .enter()
+//   .append("rect")
+//   .classed("bar", true)
+//   .attr("width", xScale.bandwidth()) // the same as style in div, attr for svg elements
+//   .attr("height", (data) => 300 - yScale(data.points)) // from max height we minus yScale to looks charts how we used to see
+//   .attr("x", (data) => xScale(data.opening)) // set width basis on function
+//   .attr("y", (data) => yScale(data.points));
 
 // const DUMMY_DATA = [
 //     { id: 'd1', value: 10, region: 'USA'},
@@ -78,4 +125,3 @@ const bars = container
 //     .attr('height', data => 300 - yScale(data.value)) // from max height we minus yScale to looks charts how we used to see
 //     .attr('x', data => xScale(data.region)) // set width basis on fucntion
 //     .attr('y', data => yScale(data.value))
-
