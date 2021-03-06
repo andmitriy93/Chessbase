58a8b63abb54fcc0b687572db3b5dafb1aceb037
Dmitrii An <an.dmitriy93@gmail.com>
2021-03-07 01:00:55 +0600

    Carousel is working only in oone direction and fix svg position

---
+++ src/black_openings.js
@@ -0,0 +1,85 @@
+// Openings for Black best 10 ------------------------------------------------------
+let defenses = d3.csvParse(
+  `Rank,Opening,Win%,Draw%,Points per 100 games
+1,Sicilian Defense,34,29,48.5
+1,Nimzo Indian,34,29,48.5
+3,Robatsch Defense,33,29,47.5
+4,Alekhine Defense,32,30,47
+5,Nimzowitsch Defense,34,25,46.5
+5,Rat,30,33,46.5
+7,Benko Gambit,32,28,46
+7,Modern Defense,31,30,46
+7,Queen's Indian Defense,25,42,46
+10,Pseudo King's Indian,29,33,45.5
+`,
+  function (d) {
+    return {
+      rank: +d["Rank"],
+      opening: d.Opening,
+      win: +d["Win%"],
+      draw: +d["Draw%"],
+      points: +d["Points per 100 games"],
+    };
+  }
+);
+
+
+let margin = { top: 20, right: 20, bottom: 30, left: 30 };
+
+let height = 500 - margin.top - margin.bottom
+let width = 1000 - margin.left - margin.right
+
+// Create svg with class name Chart
+let chart = d3.select('.black-open')
+    .attr("height", height + margin.top + margin.bottom)
+    .attr('width', width + margin.left + margin.right)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+// Create tooltip
+let Tip = tip
+            .attr('class', 'd3-tip')
+            .offset([-10, 0])
+            .html(function(event,d) {
+              return `<strong>${d.rank} Rank</strong><br>${d.win}% Win<br>${d.draw}% Draw`
+            })
+
+
+chart.call(Tip)
+
+let x = d3.scaleBand().domain(defenses.map(d => d.opening)).rangeRound([0, width]).padding(0.2)
+let y = d3.scaleLinear().domain([0, 100]).range([height, 0])
+
+// add axis
+chart.append('g').attr('class', 'axis x')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(d3.axisBottom(x).ticks(10))
+
+chart.append('g').attr('class', 'axis y')
+    .call(d3.axisLeft(y).ticks(10))
+
+// Text for bar chart
+chart.append("text")
+    // .attr("transform", "rotate(-90)")
+    .attr("y", 20)
+    // .attr("dy", "0.71em")
+    .attr('x', 500)
+    .attr("text-anchor", "end")
+    .attr('font-size', 40)
+    .text("Black best openings");
+
+let bar = chart.selectAll('.bar')
+    .data(defenses)
+    .enter()
+
+
+bar.append('rect')
+    .attr('class', 'bar')
+    .attr('x', (data) => x(data.opening))
+    .attr('y', (data) => y(data.points))
+    .attr('height',(data) => height - y(data.points))
+    .attr('width', x.bandwidth())
+    .on('mouseover', Tip.show)
+    .on('mouseout', Tip.hide)
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
+const track = document.querySelector(".carousel_track");
+const slides = Array.from(track.children); // grouping all together all li into slides
+const nextButton = document.querySelector(".carousel-button-right");
+const prevButton = document.querySelector(".carousel-button-left");
+const dotsNav = document.querySelector(".carousel_nav");
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+// arrange the slides next to one another
+const setSlidePosition = (slide, index) => {
+  slide.style.left = slideWidth * index + "px";
+};
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+  track.style.transform = "translateX(-" + targetSlide.style.left + ")";
+  currentSlide.classList.remove("current-slide");
+  targetSlide.classList.add("current-slide");
+};
+
+// when click left, move slides to the left
+
+// when click right, move slides to the right
+nextButton.addEventListener("click", (e) => {
+  const currentSlide = track.querySelector(".current-slide");
+  const nextSlide = currentSlide.nextElementSibling;
+
+  moveToSlide(track, currentSlide, nextSlide);
+});
+
+// when click nav indicator, move to that slide
+
 // // Openings for White best 10
 // let openings = d3.csvParse(
 //   `Rank,Opening,Win%,Draw%,Points per 100 games
@@ -43,7 +76,6 @@
 //               return `<strong>${d.rank} Rank</strong><br>${d.win}% Win<br>${d.draw}% Draw`
 //             })
 
-
 // chart.call(Tip)
 
 // let x = d3.scaleBand().domain(openings.map(d => d.opening)).rangeRound([0, width]).padding(0.2)
@@ -71,7 +103,6 @@
 //     .data(openings)
 //     .enter()
 
-
 // bar.append('rect')
 //     .attr('class', 'bar')
 //     .attr('x', (data) => x(data.opening))
@@ -81,96 +112,86 @@
 //     .on('mouseover', Tip.show)
 //     .on('mouseout', Tip.hide)
 
+// // Openings for Black best 10 ------------------------------------------------------
+// let defenses = d3.csvParse(
+//   `Rank,Opening,Win%,Draw%,Points per 100 games
+// 1,Sicilian Defense,34,29,48.5
+// 1,Nimzo Indian,34,29,48.5
+// 3,Robatsch Defense,33,29,47.5
+// 4,Alekhine Defense,32,30,47
+// 5,Nimzowitsch Defense,34,25,46.5
+// 5,Rat,30,33,46.5
+// 7,Benko Gambit,32,28,46
+// 7,Modern Defense,31,30,46
+// 7,Queen's Indian Defense,25,42,46
+// 10,Pseudo King's Indian,29,33,45.5
+// `,
+//   function (d) {
+//     return {
+//       rank: +d["Rank"],
+//       opening: d.Opening,
+//       win: +d["Win%"],
+//       draw: +d["Draw%"],
+//       points: +d["Points per 100 games"],
+//     };
+//   }
+// );
 
+// let margin = { top: 20, right: 20, bottom: 30, left: 30 };
 
+// let height = 500 - margin.top - margin.bottom
+// let width = 1000 - margin.left - margin.right
 
-// Openings for Black best 10 ------------------------------------------------------
-let defenses = d3.csvParse(
-  `Rank,Opening,Win%,Draw%,Points per 100 games
-1,Sicilian Defense,34,29,48.5
-1,Nimzo Indian,34,29,48.5
-3,Robatsch Defense,33,29,47.5
-4,Alekhine Defense,32,30,47
-5,Nimzowitsch Defense,34,25,46.5
-5,Rat,30,33,46.5
-7,Benko Gambit,32,28,46
-7,Modern Defense,31,30,46
-7,Queen's Indian Defense,25,42,46
-10,Pseudo King's Indian,29,33,45.5
-`,
-  function (d) {
-    return {
-      rank: +d["Rank"],
-      opening: d.Opening,
-      win: +d["Win%"],
-      draw: +d["Draw%"],
-      points: +d["Points per 100 games"],
-    };
-  }
-);
+// // Create svg with class name Chart
+// let chart = d3.select('.black-open')
+//     .attr("height", height + margin.top + margin.bottom)
+//     .attr('width', width + margin.left + margin.right)
+//     .append('g')
+//     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
+// // Create tooltip
+// let Tip = tip
+//             .attr('class', 'd3-tip')
+//             .offset([-10, 0])
+//             .html(function(event,d) {
+//               return `<strong>${d.rank} Rank</strong><br>${d.win}% Win<br>${d.draw}% Draw`
+//             })
 
-let margin = { top: 20, right: 20, bottom: 30, left: 30 };
+// chart.call(Tip)
 
-let height = 500 - margin.top - margin.bottom
-let width = 1000 - margin.left - margin.right
+// let x = d3.scaleBand().domain(defenses.map(d => d.opening)).rangeRound([0, width]).padding(0.2)
+// let y = d3.scaleLinear().domain([0, 100]).range([height, 0])
 
-// Create svg with class name Chart
-let chart = d3.select('.black-open')
-    .attr("height", height + margin.top + margin.bottom)
-    .attr('width', width + margin.left + margin.right)
-    .append('g')
-    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+// // add axis
+// chart.append('g').attr('class', 'axis x')
+//     .attr('transform', 'translate(0,' + height + ')')
+//     .call(d3.axisBottom(x).ticks(10))
 
-// Create tooltip
-let Tip = tip
-            .attr('class', 'd3-tip')
-            .offset([-10, 0])
-            .html(function(event,d) {
-              return `<strong>${d.rank} Rank</strong><br>${d.win}% Win<br>${d.draw}% Draw`
-            })
+// chart.append('g').attr('class', 'axis y')
+//     .call(d3.axisLeft(y).ticks(10))
 
+// // Text for bar chart
+// chart.append("text")
+//     // .attr("transform", "rotate(-90)")
+//     .attr("y", 20)
+//     // .attr("dy", "0.71em")
+//     .attr('x', 500)
+//     .attr("text-anchor", "end")
+//     .attr('font-size', 40)
+//     .text("Black best openings");
 
-chart.call(Tip)
+// let bar = chart.selectAll('.bar')
+//     .data(defenses)
+//     .enter()
 
-let x = d3.scaleBand().domain(defenses.map(d => d.opening)).rangeRound([0, width]).padding(0.2)
-let y = d3.scaleLinear().domain([0, 100]).range([height, 0])
-
-// add axis
-chart.append('g').attr('class', 'axis x')
-    .attr('transform', 'translate(0,' + height + ')')
-    .call(d3.axisBottom(x).ticks(10))
-
-chart.append('g').attr('class', 'axis y')
-    .call(d3.axisLeft(y).ticks(10))
-
-// Text for bar chart
-chart.append("text")
-    // .attr("transform", "rotate(-90)")
-    .attr("y", 20)
-    // .attr("dy", "0.71em")
-    .attr('x', 500)
-    .attr("text-anchor", "end")
-    .attr('font-size', 40)
-    .text("Black best openings");
-
-let bar = chart.selectAll('.bar')
-    .data(defenses)
-    .enter()
-
-
-bar.append('rect')
-    .attr('class', 'bar')
-    .attr('x', (data) => x(data.opening))
-    .attr('y', (data) => y(data.points))
-    .attr('height',(data) => height - y(data.points))
-    .attr('width', x.bandwidth())
-    .on('mouseover', Tip.show)
-    .on('mouseout', Tip.hide)
-
-
-
-
+// bar.append('rect')
+//     .attr('class', 'bar')
+//     .attr('x', (data) => x(data.opening))
+//     .attr('y', (data) => y(data.points))
+//     .attr('height',(data) => height - y(data.points))
+//     .attr('width', x.bandwidth())
+//     .on('mouseover', Tip.show)
+//     .on('mouseout', Tip.hide)
 
 // const xScale = d3
 //   .scaleBand() // ordinal scale every iteam have the same width
