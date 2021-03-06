43dc846528e65bf89b60be93e0de7b36fc81a115
Dmitrii An <an.dmitriy93@gmail.com>
2021-03-07 02:15:14 +0600

    Finish with carousel works perfectly

--- src/index.js
+++ src/index.js
@@ -19,40 +19,68 @@ const moveToSlide = (track, currentSlide, targetSlide) => {
   targetSlide.classList.add("current-slide");
 };
 
+const updateDots = (currentDot, targetDot) => {
+  currentDot.classList.remove("current-slide");
+  targetDot.classList.add("current-slide");
+};
+
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+  if (targetIndex === 0) {
+    prevButton.classList.add("is-hidden");
+    nextButton.classList.remove("is-hidden");
+  } else if (targetIndex === slides.length - 1) {
+    prevButton.classList.remove("is-hidden");
+    nextButton.classList.add("is-hidden");
+  } else {
+    prevButton.classList.remove("is-hidden");
+    nextButton.classList.remove("is-hidden");
+  }
+}
+
 // when click left, move slides to the left
 prevButton.addEventListener("click", (e) => {
   const currentSlide = track.querySelector(".current-slide");
   const prevSlide = currentSlide.previousElementSibling;
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const prevDot = currentDot.previousElementSibling;
+  const prevIndex = slides.findIndex(slide => slide === prevSlide)
 
   moveToSlide(track, currentSlide, prevSlide);
+  updateDots(currentDot, prevDot);
+  hideShowArrows(slides, prevButton, nextButton, prevIndex)
 });
 
 // when click right, move slides to the right
 nextButton.addEventListener("click", (e) => {
   const currentSlide = track.querySelector(".current-slide");
   const nextSlide = currentSlide.nextElementSibling;
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const nextDot = currentDot.nextElementSibling;
+  const nextIndex = slides.findIndex(slide => slide === nextSlide)
 
   moveToSlide(track, currentSlide, nextSlide);
+  updateDots(currentDot, nextDot);
+  hideShowArrows(slides, prevButton, nextButton, nextIndex)
 });
 
 // when click nav indicator, move to that slide
-dotsNav.addEventListener('click', e => {
+dotsNav.addEventListener("click", (e) => {
   // what indicator was clicked on?
   // click only for buttons not for the whole div
-  const targetDot = e.target.closest('button');
+  const targetDot = e.target.closest("button");
 
   if (!targetDot) return;
 
-  const currentSlide = track.querySelector('.current-slide');
-  const currentDot = dotsNav.querySelector('.current-slide');
-  const targetIndex = dots.findIndex(dot => dot === targetDot);
+  const currentSlide = track.querySelector(".current-slide");
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const targetIndex = dots.findIndex((dot) => dot === targetDot);
   const targetSlide = slides[targetIndex];
 
   moveToSlide(track, currentSlide, targetSlide);
-  // console.log(targetIndex)
-})
+  updateDots(currentDot, targetDot);
+  hideShowArrows(slides, prevButton, nextButton, targetIndex)
 
-
+});
 
 // // Openings for White best 10
 // let openings = d3.csvParse(
